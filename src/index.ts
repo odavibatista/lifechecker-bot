76845ba0d93adf1988612db028a99aa30dc54c1b
Davi Batista <davideosmar11@gmail.com>
2024-02-17 17:40:23 -0300

    apis(Lifecheck): added lifechecking on the APIs

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ export * from "colors";
 import Logging from "./library/Logging";
 
 import colors from "./colors.json";
+import { DANIELS_API_URL, TRATTORIA_API_URL } from "./config/config";
 
 const client = new ExtendedClient();
 
@@ -10,6 +11,16 @@ client.start();
 
 export { client, colors };
 
-client.on("ready", () => {
+client.on("ready", async () => {
   Logging.info(`Logged in as ${client.user?.displayName}`);
+
+  while (true)    {
+    const rand = Math.floor(Math.random() * 300000000) + 1
+    
+    if(rand == 1) {
+        await fetch(DANIELS_API_URL + "/ping")
+        await fetch(TRATTORIA_API_URL + "/dishes")
+        console.log("Healthchecks done!")
+    }
+  }
 });
